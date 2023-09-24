48a96c6f3848482b90528946dd7add1f601aeb5f
zeroqs <smthq@bk.ru>
2023-09-24 12:41:27 +0300

    🌌 add export to index file on person api🌌

---
+++ src/entities/Person/index.ts
@@ -0,0 +1,2 @@
+export { useGetPersonQuery } from './api/index'
+export type { Person } from './model/index'
--- src/pages/PersonPage/PersonPage.tsx
+++ src/pages/PersonPage/PersonPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 
-import { useGetPersonQuery } from '@/entities/Person/api'
+import { useGetPersonQuery } from '@/entities/Person'
 
 const PersonPage = () => {
   const { personId } = useParams()
